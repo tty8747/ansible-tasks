cce7377ab93b4ac1ee72a7ff5b614c82a8760eb1
Sergey Chulanov <tty8747@gmail.com>
2021-12-13 14:56:45 +0300

    Added list of nodes

--- nodes.tf
+++ nodes.tf
@@ -2,3 +2,38 @@ resource "aws_key_pair" "ansible-key" {
   key_name   = "ansible-key"
   public_key = file(var.path_to_mykey)
 }
+
+data "aws_ami" "centos7" {
+  most_recent = true
+  owners      = ["aws-marketplace"]
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "image-type"
+    values = ["machine"]
+  }
+}
+
+resource "aws_instance" "node" {
+  count = length(var.instances)
+  ami           = data.aws_ami.centos7.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "${var.instances[count.index]}"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "myout" {
   value = aws_key_pair.ansible-key.id
 }
+
+output "ami" {
+  value = data.aws_ami.centos7.id
+}
