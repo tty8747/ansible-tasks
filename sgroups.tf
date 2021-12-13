f397cd3a7e2cfb71f0a48d920d1b4221ec95f1d5
Sergey Chulanov <tty8747@gmail.com>
2021-12-13 21:56:17 +0300

    Modifed security groups

--- sgroups.tf
+++ sgroups.tf
@@ -2,18 +2,22 @@ resource "aws_security_group" "aws_sg" {
   name        = "mysg"
   description = "Allow all"
   vpc_id      = aws_vpc.my_vpc.id
+}
 
-  ingress {
-    from_port        = "0"
-    to_port          = "0"
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "ingress_all" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0 
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.aws_sg.id
+}
 
-  egress {
-    from_port        = "0"
-    to_port          = "0"
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0 
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.aws_sg.id
 }
